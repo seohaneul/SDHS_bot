4ebf1ce41b93df5d86258d9bed1a8f44ebc16b92
seohaneul <seo1533618@gmail.com>
2024-01-11 16:30:58 +0900

    commit

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const app = express();
 
-var indexRouter = require("./routes/index");
-var keyboardRouter = require("./routes/keyboard");
-var messageRouter = require("./routes/message");
-
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-app.use("/", indexRouter);
-app.use("/keyboard", keyboardRouter);
-app.use("/message", messageRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.get("/keyboard", (req, res) => {
+  const data = { type: "text" };
+  res.json(data);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.post("/message", (req, res) => {
+  const question = req.body.userRequest.utterance;
+  const goMain = "처음으로";
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  if (question === "테스트") {
+    const data = {
+      version: "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: "테스트",
+            },
+          },
+        ],
+        quickReplies: [
+          {
+            label: goMain,
+            action: "message",
+            messageText: goMain,
+          },
+        ],
+      },
+    };
+  }
+  res.json(data);
 });
 
-module.exports = app;
+app.listen(3000, () => console.log("node on 3000"));
